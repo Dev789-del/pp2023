7596efcd0e4e5f2f3c94f754c6e6de16e385f3a5
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-02-17 10:54:46 +0700

    Modify mark_subject function and add function to display student mark

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,6 +1,6 @@
 courses_information = {}
 students_information = {}
-mark_subject_single = {}
+mark_subject_list = {}
 
 def student_info():
     #This function ask user to input information for a specific number of students
@@ -26,25 +26,22 @@ def course_info():
     for j in range(course_number):
         course_id = str(input("Enter course's id: "))
         course_name = str(input("Enter course's name: "))
+        courses_information[course_id] = {'Course_Name': course_name}
 def mark_subject():
     #Here is the function to input mark for student 
     course_check = str(input("Please enter course's id/course's name to fill mark: "))
-    while course_check not in course_list_info:
+    while course_check not in courses_information:
         course_check = str(input("Please reenter the valid course's id/course's name in course list info: "))
-    if course_check in course_list_info:
+    if course_check in courses_information:
         print("You enter the right course's id.Please continue.")
-        mark_subject_single.append(course_check)
-    student_check = str(input("Please enter student's id or student's name to check if it exists: "))
-    while student_check not in student_list_info:
-        student_check = str(input("Please reenter student's name or id to access next step: "))
-    if student_check in student_list_info:
-        mark_subject_single.append(student_check)
-    mark_student = float(input("Please input positive mark for subject: "))
-    while mark_student < 0:
-        mark_student = float(input("Please reenter the right positive number for subject's mark: "))
-    if mark_student > 0:
-        mark_subject_single.append(mark_student)
-    return mark_subject_single    
+    for student_id in students_information:
+        mark_student = float(input(f"Enter marks for student: {mark_student}"))
+        while mark_student < 0 and mark_student > 20:
+            mark_student = float(input(f"You need to enter the mark again: {mark_student}"))
+        else: 
+            if student_id not in mark_subject_list:
+                mark_subject_list[student_id] = {}
+            mark_subject_list[student_id][course_check] = mark_subject_list
 #Define functions to show list of courses and list of students
 def courses_list():
     print("Here is the list of courses information")
@@ -52,7 +49,21 @@ def courses_list():
 def students_list():
     print("Below is the list of students information")
     print(students_information)
+#Function below to present mark transcipt for student
+def mark_receive():
+    course_identify = input("Enter course ID/course name to check: ")
+    while course_identify not in courses_information:
+        print("Course ID/Course name not exist.Please enter again")
+    else: 
+        print("You enter the valid input")
+    for student_id in students_information:
+        if student_id in mark_subject_list and course_identify in mark_subject_list[student_id]:
+            print(f"{students_information[student_id]['Student_Name']}: {mark_subject_list[student_id][course_identify]}")
+        else:
+            print(f"{students_information[student_id]['Student_Name']}: N/A")
 #While loop to show list of choices
+student_info()
+course_info()
 Condition = True
 while Condition:
     print("----------Student Management System welcome users----------")
