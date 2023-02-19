2dba3abb8a5568bbf5e28e58892e4922541b6820
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-02-19 15:29:20 +0700

    Fix problem in 2 functions mark_subject and mark_receive

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -38,45 +38,46 @@ def mark_subject():
     #Here is the function to input mark for students
     course_check = str(input("Please enter course's ID to fill mark: "))
     while course_check not in courses_information:
-        course_check = str(input("Please reenter the correct course's ID in course list information: "))
+        course_check = str(input("Please reenter the correct course's ID in course list's information: "))
     if course_check in courses_information:
         print("You enter the correct course's ID.")
     for student_ID in students_information:
         mark_student = float(input(f"Enter mark for student {students_information[student_ID]['Student_Name']}: "))
-        while mark_student < 0 and mark_student > 20:
+        while mark_student < 0.0 and mark_student > 20.0:
             mark_student = float(input(f"You need to enter the mark again for student {students_information[student_ID]['Student_Name']}: "))
         else: 
             if student_ID not in mark_subject_list:
                 mark_subject_list[student_ID] = {}
-            mark_subject_list[student_ID][course_check] = mark_subject_list
+                mark_subject_list[student_ID][course_check] = mark_student
     mark_subject_time = datetime.datetime.now()
     print(f"You had input information for subject mark at {mark_subject_time}")
 
 #Define functions to show list of courses and list of students
 def courses_list():
     courses_list_time = datetime.datetime.now()
-    print(f"Here is the list of courses information after select option 1 at {courses_list_time}:")
+    print(f"Here is the list of courses information after selecting option 1 at {courses_list_time}:")
     print(courses_information)
 def students_list():
     students_list_time = datetime.datetime.now()
-    print(f"Here is the list of students information after select option 2 at {students_list_time}:")
+    print(f"Here is the list of students information after selecting option 2 at {students_list_time}:")
     print(students_information)
 
 #Function below to present mark transcipt for student
 def mark_receive():
-    course_identify = input("Enter course ID/course name to check: ")
+    course_identify = input("Enter course's ID to check: ")
     while course_identify not in courses_information:
-        print("Course ID/Course name not exist.Please enter again")
+        print("Course's ID not exist.Please enter again")
     else: 
-        print("You enter the valid input")
+        print("You enter the valid course's ID")
+    mark_receive_time = datetime.datetime.now()
     print(f"Student Mark return at {mark_receive_time}")
     for student_ID in students_information:
         if student_ID in mark_subject_list and course_identify in mark_subject_list[student_ID]:
+            print(f"----------Course {course_identify} final mark----------")
             print(f"{students_information[student_ID]['Student_Name']}: {mark_subject_list[student_ID][course_identify]}")
         else:
             print(f"{students_information[student_ID]['Student_Name']}: N/A")
-    mark_receive_time = datetime.datetime.now()
-    
+  
 #Executing student_info and course_info function
 student_info()
 course_info()
